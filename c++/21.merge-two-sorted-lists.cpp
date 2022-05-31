1ace5134db1ed6a725d9f0866c9f09084b7612b9
si <si@sis-MacBook-Pro.local>
2022-05-31 12:34:49 -0600

    finshed 21

---
+++ c++/21.merge-two-sorted-lists.cpp
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode id=21 lang=cpp
+ *
+ * [21] Merge Two Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// struct ListNode {
+// 	int val;
+// 	ListNode *next;
+// 	ListNode() : val(0), next(nullptr) {}
+// 	ListNode(int x) : val(x), next(nullptr) {}
+// 	ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+
+class Solution {
+public:
+	// time : O(n)
+	// space: O(1)
+	// 208/208 cases passed (13 ms)
+	// Your runtime beats 36.26 % of cpp submissions
+	// Your memory usage beats 82.01 % of cpp submissions (14.7 MB)
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+		if (!list1 && !list2) return (ListNode *) nullptr;
+		if (!list1) return list2;
+		if (!list2) return list1;
+		ListNode * t1 = list1, *t2 = list2;
+		ListNode * merged;
+		if (t1 -> val > t2 -> val) {
+			merged = t2;
+			t2 = t2 -> next;
+			merged -> next = nullptr;
+		} else {
+			merged = t1;
+			t1 = t1 -> next;
+			merged -> next = nullptr;
+		}
+
+		ListNode *mtemp = merged;
+		while (t1 && t2) {
+			if (t1 -> val > t2 -> val) {
+				mtemp -> next = t2;
+				t2 = t2 -> next;
+				mtemp = mtemp -> next;
+				mtemp -> next = nullptr;
+			} else {
+				mtemp -> next = t1;
+				t1 = t1 -> next;
+				mtemp = mtemp -> next;
+				mtemp -> next = nullptr;
+			}
+		}
+		if (!t1 && t2) mtemp -> next = t2;
+		else if (!t2 && t1) mtemp -> next =t1;
+		return merged;
+    }
+};
+// @lc code=end
+
