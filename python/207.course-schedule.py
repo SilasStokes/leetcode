4564fea3bb421d593fd92bbf06f7749a96d92d8e
si <si@sis-MacBook-Pro.lan>
2022-07-12 19:58:44 -0700

    207 course schedule

---
+++ python/207.course-schedule.py
@@ -0,0 +1,94 @@
+#
+# @lc app=leetcode id=207 lang=python3
+# @lc code=start
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        schedule = {}
+
+        for i in range(numCourses):
+            schedule[i] = []
+        
+        for cls, pre in prerequisites:
+            schedule[cls].append(pre)
+
+        visited = set()
+
+        def dfs(key):
+            if key in visited:
+                return False
+
+            if schedule[key] == []:
+                return True
+
+            visited.add(key)
+
+            for cls in schedule[key]:
+                if not dfs(cls): return False
+
+            visited.remove(key)
+            schedule[key] = []
+            return True
+
+        for cls in range(numCourses):
+            if not dfs(cls): return False
+        return True
+# @lc code=end
+
+
+#
+# [207] Course Schedule
+#
+# https://leetcode.com/problems/course-schedule/description/
+#
+# algorithms
+# Medium (45.08%)
+# Likes:    10154
+# Dislikes: 408
+# Total Accepted:    926.4K
+# Total Submissions: 2.1M
+# Testcase Example:  '2\n[[1,0]]'
+#
+# There are a total of numCourses courses you have to take, labeled from 0 to
+# numCourses - 1. You are given an array prerequisites where prerequisites[i] =
+# [ai, bi] indicates that you must take course bi first if you want to take
+# course ai.
+# 
+# 
+# For example, the pair [0, 1], indicates that to take course 0 you have to
+# first take course 1.
+# 
+# 
+# Return true if you can finish all courses. Otherwise, return false.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: numCourses = 2, prerequisites = [[1,0]]
+# Output: true
+# Explanation: There are a total of 2 courses to take. 
+# To take course 1 you should have finished course 0. So it is possible.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+# Output: false
+# Explanation: There are a total of 2 courses to take. 
+# To take course 1 you should have finished course 0, and to take course 0 you
+# should also have finished course 1. So it is impossible.
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= numCourses <= 2000
+# 0 <= prerequisites.length <= 5000
+# prerequisites[i].length == 2
+# 0 <= ai, bi < numCourses
+# All the pairs prerequisites[i] are unique.
+# 
+# 
+#
